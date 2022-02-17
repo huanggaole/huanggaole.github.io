15d128c9e55d099c33080f37ab3b02ada022b95d
高乐 黄 <752769455@qq.com>
2022-02-18 01:15:44 +0800

    no message

---
+++ autoRMmap/src/settings.8c3f4.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/mainScene.fire",orientation:"",jsList:[],bundleVers:{internal:"69b9a",resources:"78d20",main:"3df9a"}};
