33b3c2e770210cef4830361979ad29cd3900c95b
高乐 黄 <752769455@qq.com>
2024-12-31 02:21:34 +0800

    feat: 添加彩虹熊熊demo

---
+++ rainbowbear/src/settings.ae83f.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Main.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"d17e2",main:"8c5f3"}};
